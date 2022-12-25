0eaa833b1821509fb2fbbc64beb70b49887c294a
Peiyun <b612starnight@gmail.com>
2022-12-25 21:21:09 +0800

    finish

--- myGifView/ViewController.swift
+++ myGifView/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var gif = ["1","2","3","4","5"]
+    
+    @IBOutlet weak var myGifView: UIImageView!
+
+    @IBAction func change(_ sender: UIButton) {
+        
+        let number = Int.random(in: 0...4)
+
+        myGifView.loadGif(name: gif[number])
+
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        myGifView.loadGif(name: gif[0])
+        
     }
-
-
 }
 
